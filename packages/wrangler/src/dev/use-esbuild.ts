2400ad6c06ab6ce2e898d945e015593b49f64baf
Peter Bacon Darwin <pete@bacondarwin.com>
2023-08-09 13:34:10 +0100

    refactor to simplify findAdditionalModules()

--- packages/wrangler/src/dev/use-esbuild.ts
+++ packages/wrangler/src/dev/use-esbuild.ts
@@ -4,22 +4,23 @@ import { useApp } from "ink";
 import { useState, useEffect } from "react";
 import { rewriteNodeCompatBuildFailure } from "../deployment-bundle/build-failures";
 import { bundleWorker } from "../deployment-bundle/bundle";
+import { getBundleType } from "../deployment-bundle/bundle-type";
 import { dedupeModulesByName } from "../deployment-bundle/dedupe-modules";
 import findAdditionalModules from "../deployment-bundle/find-additional-modules";
 import { logBuildFailure, logger } from "../logger";
 import type { Config } from "../config";
 import type { SourceMapMetadata } from "../deployment-bundle/bundle";
 import type { Entry } from "../deployment-bundle/entry";
-import type { CfModule } from "../deployment-bundle/worker";
+import type { ModuleCollector } from "../deployment-bundle/module-collection";
+import type { CfModule, CfModuleType } from "../deployment-bundle/worker";
 import type { WorkerRegistry } from "../dev-registry";
-import type { ModuleCollector } from "../module-collection";
 import type { WatchMode, Metafile } from "esbuild";
 
 export type EsbuildBundle = {
 	id: number;
 	path: string;
 	entry: Entry;
-	type: "esm" | "commonjs";
+	type: CfModuleType;
 	modules: CfModule[];
 	dependencies: Metafile["outputs"][string]["inputs"];
 	sourceMapPath: string | undefined;
@@ -116,14 +117,14 @@ export function useEsbuild({
 		async function build() {
 			if (!destination) return;
 
-			let traverseModuleGraphResult:
-				| Awaited<ReturnType<typeof bundleWorker>>
-				| undefined;
-			let bundleResult: Awaited<ReturnType<typeof bundleWorker>> | undefined;
-			if (noBundle) {
-				traverseModuleGraphResult = await findAdditionalModules(entry, rules);
-			}
+			const newAdditionalModules = noBundle
+				? dedupeModulesByName([
+						...((await findAdditionalModules(entry, rules)) ?? []),
+						...additionalModules,
+				  ])
+				: additionalModules;
 
+			let bundleResult: Awaited<ReturnType<typeof bundleWorker>> | undefined;
 			if (processEntrypoint || !noBundle) {
 				bundleResult = await bundleWorker(entry, destination, {
 					bundle: !noBundle,
@@ -148,10 +149,7 @@ export function useEsbuild({
 					firstPartyWorkerDevFacade,
 					targetConsumer,
 					testScheduled,
-					additionalModules: dedupeModulesByName([
-						...(traverseModuleGraphResult?.modules ?? []),
-						...additionalModules,
-					]),
+					additionalModules: newAdditionalModules,
 				});
 			}
 
@@ -176,15 +174,8 @@ export function useEsbuild({
 					id: 0,
 					entry,
 					path: bundleResult?.resolvedEntryPointPath ?? entry.file,
-					type:
-						bundleResult?.bundleType ??
-						(entry.format === "modules" ? "esm" : "commonjs"),
-					modules: bundleResult
-						? bundleResult.modules
-						: dedupeModulesByName([
-								...(traverseModuleGraphResult?.modules ?? []),
-								...additionalModules,
-						  ]),
+					type: bundleResult?.bundleType ?? getBundleType(entry.format),
+					modules: bundleResult ? bundleResult.modules : newAdditionalModules,
 					dependencies: bundleResult?.dependencies ?? {},
 					sourceMapPath: bundleResult?.sourceMapPath,
 					sourceMapMetadata: bundleResult?.sourceMapMetadata,
